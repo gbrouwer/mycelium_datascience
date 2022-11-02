22d509c1bb218e94f5341dae5296c4b7899a4f7d
Gijs Joost Brouwer <gbrouwer5151@gmail.com>
2022-11-02 13:24:57 -0400

    bulk commit of existing files

---
+++ js/toc.js
@@ -0,0 +1,15 @@
+let toc_content = '<ul className="nolist"> ' +
+'<li> ' +
+'<a href="">Chapter 1 - Foreword</a> '+
+'<ul className="nolist"> ' +
+'<li><a href="chapter_001.html#1.1">1.1 - Another book on machine learning?</a></li> ' +
+'<li><a href="chapter_001.html#1.2">1.2 - Concepts, abstractions, and intuitions</a></li> ' +
+'<li><a href="chapter_001.html#1.3">1.3 – Outline</a></li> ' +
+'<li><a href="chapter_001.html#1.4">1.4 - Examples and demos</a></li> ' +
+'<li><a href="chapter_001.html#1.5">1.5 - Errata</a></li>' +
+'<li><a href="chapter_001.html#1.6">1.6 – References</a></li>' +
+'</li>' +
+'</ul>';
+
+
+
